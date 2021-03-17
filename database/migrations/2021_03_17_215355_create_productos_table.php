731cedf2195d77d30899a82b41078a2bdc5d3352
Kami <kami.kkz.kamikaze@gmail.com>
2021-03-17 16:13:14 -0600

    Preparativos iniciales

---
+++ database/migrations/2021_03_17_215355_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('productName');
+            $table->float('productPrice');
+            $table->string('productImgUrl');
+            $table->text('productCode');
+            $table->integer('productPosition');
+            $table->timestamps();
+            $table->integer('categoria_id')->unsigned();
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('content')
+
+<div class="container">
+    <h1>Hola mundo</h1>
+</div>
+
+@endsection
