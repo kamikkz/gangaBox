d986c46f3cecf889d119eecea32872b91998cbb1
Kami <kami.kkz.kamikaze@gmail.com>
2021-03-19 08:56:08 -0600

    BugFix Librerias para update  Bootstrap

--- resources/views/livewire/headerDashboard.blade.php
+++ resources/views/livewire/headerDashboard.blade.php
@@ -1,28 +1,26 @@
 <div>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand">JORGE SUAREZ</a>
+      <a class="navbar-brand">TEST</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/categorias') }}">Categorías</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/productos') }}">Productos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/operations') }}">Orden de Productos</a>
+              </li>
             @foreach ($categorias as $categoria)
             <li class="nav-item">
-                <button wire:click="show({{$categoria->id}})" class="btn btn-outline-warning" >
-                    {{$categoria->categoryName}}
-                </button>
+                <a class="nav-link" wire:click="show({{$categoria->id}})" href="#">{{$categoria->categoryName}}</a>
             </li>
             @endforeach
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/categorias') }}">Categorías</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/productos') }}">Productos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ url('/operations') }}">Orden de Productos</a>
-          </li>
         </ul>
       </div>
     </div>
